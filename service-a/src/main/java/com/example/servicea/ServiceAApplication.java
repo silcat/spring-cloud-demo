9ebaebbe0f32bac182bc960e52a451391b03d92e
yangtianfeng <yangtianfeng@youxin.com>
2018-10-19 18:49:12 +0800

    "监控模板"

--- service-a/src/main/java/com/example/servicea/ServiceAApplication.java
+++ service-a/src/main/java/com/example/servicea/ServiceAApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,6 +17,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableFeignClients
 @EnableRabbit
+@EnableHystrixDashboard
 public class ServiceAApplication {
 
     public static void main(String[] args) {
