34e227032242ef7908bcdbebc8c6e49fd2dc4ac8
yangtianfeng <yangtianfeng@youxin.com>
2018-10-29 19:08:15 +0800

    "集成zipkin"

--- hytrixs/src/main/java/com/example/hytrixs/HytrixsApplication.java
+++ hytrixs/src/main/java/com/example/hytrixs/HytrixsApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
+import zipkin.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
 @EnableTurbineStream
 @EnableHystrixDashboard
 @EnableDiscoveryClient
+@EnableZipkinServer
 public class HytrixsApplication {
 
     public static void main(String[] args) {
