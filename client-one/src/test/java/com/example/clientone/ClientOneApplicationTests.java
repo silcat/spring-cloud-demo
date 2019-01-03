4b6385f3750fc78513bbe9bef823442c0282a159
yangtianfeng <yangtianfeng@youxin.com>
2019-01-03 14:32:53 +0800

    admin

--- client-one/src/test/java/com/example/clientone/ClientOneApplicationTests.java
+++ client-one/src/test/java/com/example/clientone/ClientOneApplicationTests.java
@@ -5,12 +5,16 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+
 @SpringBootTest
 public class ClientOneApplicationTests {
 
     @Test
     public void contextLoads() {
+        float v = Float.parseFloat("77.9");
+
+        float sd = Float.parseFloat("0");
+
     }
 
 }
