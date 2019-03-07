61c9725227f8926d5c0161e66db5d11f7311d3f0
yangtianfeng <yangtianfeng@youxin.com>
2019-03-07 19:00:50 +0800

    "权限校验"

--- monitor/src/main/java/com/example/monitor/configuration/SecurityConfig.java
+++ monitor/src/main/java/com/example/monitor/configuration/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.example.monitor.configuration;
 
+import de.codecentric.boot.admin.config.AdminServerProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -7,24 +10,29 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 @EnableWebSecurity
+@EnableConfigurationProperties(AdminServerProperties.class)
 public  class SecurityConfig extends WebSecurityConfigurerAdapter  {
+
+    @Autowired
+    private AdminServerProperties adminServerProperties;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // Page with login form is served as /login.html and does a POST on /login
         http.formLogin().loginPage("/login.html").loginProcessingUrl("/login").permitAll();
         // The UI does a POST on /logout on logout
-        http.logout().logoutUrl("/monitor/logout");
+        http.logout().logoutUrl(adminServerProperties.getContextPath()+"/logout");
         // The ui currently doesn't support csrf
         http.csrf().disable();
 
-        // Requests for the login page and the static assets are allowed
+//        // Requests for the login page and the static assets are allowed
         http.authorizeRequests()
-                .antMatchers("/login.html", "/**/*.css", "/img/**", "/third-party/**")
+                .antMatchers("/login.html","/**/*.css", "/img/**", "/third-party/**")
                 .permitAll();
-        // ... and any other request needs to be authorized
-        http.authorizeRequests().antMatchers("/**").authenticated();
 
+        // ... and any other request needs to be authorized
+        http.authorizeRequests().antMatchers(adminServerProperties.getContextPath()+"/**").authenticated();
+       ;
         // Enable so that the clients can authenticate via HTTP basic for registering
         http.httpBasic();
     }
